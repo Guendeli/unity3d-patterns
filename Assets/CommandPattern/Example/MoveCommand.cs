9b47587d2ffddda964d530e342d6c78038859007
GUENDELI Omar <guendeliomar@gmail.com>
2018-07-18 04:03:11 +0100

    Command Pattern

---
+++ Assets/CommandPattern/Example/MoveCommand.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MoveDirection { up, down, left, right };
+class MoveCommand : Command
+{
+    private MoveDirection _direction;
+    private float _distance;
+    private GameObject _gameObject;
+
+
+    //Constructor
+    public MoveCommand(MoveDirection direction, float distance, GameObject gameObjectToMove)
+    {
+        this._direction = direction;
+        this._distance = distance;
+        this._gameObject = gameObjectToMove;
+    }
+
+
+
+    //Execute new command
+    public override void Execute()
+    {
+        MoveOperation(_gameObject, _direction, _distance);
+    }
+
+
+    //Undo last command
+    public override void UnExecute()
+    {
+        MoveOperation(_gameObject, InverseDirection(_direction), _distance);
+    }
+
+
+    //invert the direction for undo
+    private MoveDirection InverseDirection(MoveDirection direction)
+    {
+        switch (direction)
+        {
+            case MoveDirection.up:
+                return MoveDirection.down;
+            case MoveDirection.down:
+                return MoveDirection.up;
+            case MoveDirection.left:
+                return MoveDirection.right;
+            case MoveDirection.right:
+                return MoveDirection.left;
+            default:
+                Debug.LogError("Unknown MoveDirection");
+                return MoveDirection.up;
+        }
+    }
+
+    public void MoveOperation(GameObject gameObjectToMove, MoveDirection direction, float distance)
+    {
+        switch (direction)
+        {
+            case MoveDirection.up:
+                MoveZ(gameObjectToMove, distance);
+                break;
+            case MoveDirection.down:
+                MoveZ(gameObjectToMove, -distance);
+                break;
+            case MoveDirection.left:
+                MoveX(gameObjectToMove, -distance);
+                break;
+            case MoveDirection.right:
+                MoveX(gameObjectToMove, distance);
+                break;
+        }
+    }
+
+    private void MoveZ(GameObject gameObjectToMove, float distance)
+    {
+        Vector3 newPos = gameObjectToMove.transform.position;
+        newPos.z += distance;
+        gameObjectToMove.transform.position = newPos;
+    }
+
+    private void MoveX(GameObject gameObjectToMove, float distance)
+    {
+        Vector3 newPos = gameObjectToMove.transform.position;
+        newPos.x += distance;
+        gameObjectToMove.transform.position = newPos;
+    }
+}
---
+++ Assets/CommandPattern/Example/PlayerMovementHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementHandler : MonoBehaviour
+{
+    public float moveDistance = 10f;
+    public GameObject objectToMove;
+
+    private List<Command> commands = new List<Command>();
+    private int currentCommandNum = 0;
+
+    void Start()
+    {
+        if (objectToMove == null)
+        {
+            Debug.LogError("objectToMove must be assigned via inspector");
+            this.enabled = false;
+        }
+    }
+
+    private void Move(MoveDirection direction)
+    {
+        MoveCommand moveCommand = new MoveCommand(direction, moveDistance, objectToMove);
+        moveCommand.Execute();
+        commands.Add(moveCommand);
+        currentCommandNum++;
+    }
+
+    public void Undo()
+    {
+        if (currentCommandNum > 0)
+        {
+            currentCommandNum--;
+            MoveCommand moveCommand = (MoveCommand)commands[currentCommandNum];
+            moveCommand.UnExecute();
+            commands.Remove(moveCommand);
+        }
+    }
+
+
+
+
+    //Simple move commands to attach to UI buttons
+    public void MoveUp() { Move(MoveDirection.up); }
+    public void MoveDown() { Move(MoveDirection.down); }
+    public void MoveLeft() { Move(MoveDirection.left); }
+    public void MoveRight() { Move(MoveDirection.right); }
+
+}
